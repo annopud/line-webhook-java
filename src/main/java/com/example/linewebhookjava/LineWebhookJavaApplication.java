35ce9bcbe6e6b2ab1304af3afff55e0b36f2990c
tttmb <annop.email@gmail.com>
2024-04-26 02:02:15 +0700

    first commit

---
+++ src/main/java/com/example/linewebhookjava/LineWebhookJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.linewebhookjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineWebhookJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineWebhookJavaApplication.class, args);
+    }
+
+}
